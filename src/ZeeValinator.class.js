878068a0ac6a8f4db9c1f4626aeeecf4c9122a45
Sybren Dijkstra <nerbiz@gmail.com>
2017-12-08 17:08:55 +0100

    Moved the default checks to a separate class

--- src/ZeeValinator.class.js
+++ src/ZeeValinator.class.js
@@ -2,10 +2,11 @@ function ZeeValinator() {
     var self = this;
 
     /**
-     * Class dependency
-     * @type ZeeValinatorCharacters
+     * Dependencies
+     * @type Object
      */
     self.characters = new ZeeValinatorCharacters();
+    self.defaultChecks = new ZeeValinatorDefaultChecks();
 
     /**
      * The CSS class to add to an input element that contains an error
@@ -309,71 +310,6 @@ function ZeeValinator() {
      * Set some validation checks, to be used by self.validate
      */
     (self.setChecks = function() {
-        self.newCheck({
-            alphanumeric: function(value, option) {
-                // Value needs to be alphanumeric
-                var result = value.match(/^[a-z\d]+$/gi);
-                return (result !== null);
-            },
-            email: function(value, option) {
-                // Value needs to be a valid email address
-                var result = value.match(/^.+?@.+?\..+$/g);
-                return (result !== null);
-            },
-            hexColor: function(value, option) {
-                // Value needs to be a hexadecimal color, #xxx or #xxxxxx
-                var result = value.match(/^#([a-f\d]{3}){1,2}$/gi);
-                return (result !== null);
-            },
-            length: function(value, option) {
-                // Value needs to have a fixed length
-                return (value.length == parseInt(option, 10));
-            },
-            match: function(value, option) {
-                // Value needs to match the option
-                return (value == option);
-            },
-            matchWith: function(value, option) {
-                // Value needs to match the value of another element
-                return (value == $('[name="' + option + '"]').val());
-            },
-            maxLength: function(value, option) {
-                // Value needs to have a maximum length
-                return (value.length <= parseInt(option, 10));
-            },
-            maxNumber: function(value, option) {
-                // Value needs to be a number, and lower than or equal to the option
-                return ( ! isNaN(value - parseFloat(value))  &&  value <= option);
-            },
-            minLength: function(value, option) {
-                // Value needs to have a minimum length
-                return (value.length >= parseInt(option, 10));
-            },
-            minNumber: function(value, option) {
-                // Value needs to be a number, and higher than or equal to the option
-                return ( ! isNaN(value - parseFloat(value))  &&  value >= option);
-            },
-            personName: function(value, option) {
-                // Value needs to be a valid name
-                // Remove accents, before validating the alphabetical characters
-                var nameNoAccents = self.characters.removeAccents(value);
-                var result = nameNoAccents.match(/^[a-z0-9 \/,.-]+$/gi);
-                return (result !== null);
-            },
-            numeric: function(value, option) {
-                // Value needs to be a number
-                // From: https://github.com/angular/angular/blob/4.3.x/packages/common/src/pipes/number_pipe.ts#L172
-                return ( ! isNaN(value - parseFloat(value)));
-            },
-            phone: function(value, option) {
-                // Value needs to be a valid phone number
-                var result = value.match(/^[\d\(\) \+-]+$/g);
-                return (result !== null);
-            },
-            required: function(value, option) {
-                // Value can't be empty
-                return (value != '');
-            }
-        });
+        self.newCheck(self.defaultChecks.get());
     })();
 }
